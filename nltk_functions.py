5a0e6d745350f0ccae185ca7bd2cef7e070a7556
Benjamin Meyers <meyersbs@users.noreply.github.com>
2015-06-15 15:50:12 -0400

    Added Comments. Added 'MAIN' Function.
    This is not a real main function. Instead, this is a while-loop with a breakout to terminate the program.

--- nltk_functions.py
+++ nltk_functions.py
@@ -11,6 +11,7 @@
 #########################################################################################
 from nltk.probability import FreqDist
 from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import word_tokenize
 import re
 
 ##### GLOBAL VARIABLES ###########################
@@ -55,10 +56,11 @@ TD3_p_TS = 'TD3_T2_participant_time.txt'
 TD3_p_NS = 'TD3_T2_participant_noTS.txt'
 TD3_s_TS = 'TD3_T2_Stereo_time.txt'
 TD3_s_NS = 'TD3_T2_Stereo_noTS.txt'
-##################################################
 
-#A simple NLTK function
-def get_freqs(text_file):
+##### FUNCTIONS ##################################
+
+#Display the top x most frequent tokens (words) in the given text_file.
+def get_freqs(text_file, x):
 	all_words = get_tokens(text_file)
 	new_words = []
 	for token in all_words:
@@ -76,10 +78,9 @@ def get_freqs(text_file):
 
 	freq_dist = FreqDist(new_words)
 
-	print(freq_dist.most_common(50))
+	print(freq_dist.most_common(x))
 
-#Generate Tokens from a given text file, ignoring
-#	all annotations.
+#Generate Tokens from a given text file, ignoring all annotations.
 def get_tokens(text_file):
 	f = open(text_file)
 	raw = f.read()
@@ -90,11 +91,41 @@ def get_tokens(text_file):
 ##### TEST FUNCTIONS #############################
 def check_tokenize_accuracy():
 	if get_tokens(AS1_f_NT) == get_tokens(AS1_f_NT):
-		print('SUCCESS!')
+		return 'ACCURATE!'
 	else:
-		print('FAILURE!')
+		return 'INACCURATE!'
+
+def run_accuracy_tests():
+	print('##### RUNNING TESTS ####################')
+	print('\nget_tokens(str) status: ' + check_tokenize_accuracy())
+	print('\n########################################')
+
+##### DISPLAY COMMAND LIST #######################
+def display_command_list():
+	command_list = '##################################################'
+	command_list += '\ncommand arg1 \targ2 \tdescription\n'
+	command_list += '\nf \tstr \tint \tget_freqs(str, int)'
+	command_list += '\nh \t-- \t-- \tdisplay_command_list()'
+	command_list += '\nt \tstr \t-- \tget_tokens(str)'
+	command_list += '\n! \t-- \t-- \trun_accuracy_tests()'
+	command_list += '\nq \t-- \t-- \tquit program.'
+	command_list += '\n##################################################'
+	print(command_list)
 
 ##### MAIN #######################################
-get_freqs(AS1_f_NT)
-get_freqs(AS1_f_TS)
-check_tokenize_accuracy()
+while(True):
+	user_command = raw_input('Please provide a command: ')
+	command = word_tokenize(user_command)
+	#print(command)
+	if command[0] == 't' and len(command) == 2:
+		get_tokens(command[1])
+	elif command[0] == 'f' and len(command) == 3:
+		get_freqs(command[1], int(command[2]))
+	elif command[0] == 'h' and len(command) == 1:
+		display_command_list()
+	elif command[0] == '!' and len(command) == 1:
+		run_accuracy_tests()
+	elif command[0] == 'q' and len(command) == 1:
+		break
+	else:
+		print('Invalid Command.\nFor Help, type \'h\'.')
