6376c66e290bc0e5c1c8565a389f5a63b469c435
Benjamin Meyers <meyersbs@users.noreply.github.com>
2015-06-16 15:01:27 -0400

    Pulled MAIN out of nltk_functions.py

--- nltk_functions.py
+++ nltk_functions.py
@@ -1,68 +1,65 @@
-#########################################################################################
-# File Name: nltk_functions.py
-# Date Created: 06-15-2015
-# Date Revised: 06-16-2015
-# Author: Benjamin S. Meyers
-# Email: bsm9339@rit.edu
-# 	Advisor: Emily Prud'hommeaux
-# 	Email: emilypx@rit.edu
-# 	Advisor: Cissi Ovesdotter-Alm
-# 	Email: coagla@rit.edu
-#########################################################################################
+#################################################################
+# File Name: nltk_functions.py					#
+# Date Created: 06-15-2015					#
+# Date Revised: 06-16-2015					#
+# Author: Benjamin S. Meyers					#
+# Email: bsm9339@rit.edu					#
+# 	Advisor: Emily Prud'hommeaux				#
+# 	Email: emilypx@rit.edu					#
+# 	Advisor: Cissi Ovesdotter-Alm				#
+# 	Email: coagla@rit.edu					#
+#################################################################
+from nltk.corpus import gutenberg
 from nltk.probability import FreqDist
 from nltk.tokenize import RegexpTokenizer
 from nltk.tokenize import word_tokenize
+import nltk
 import random
 import re
 
-##### GLOBAL VARIABLES ###########################
-AS1_f_TS = 'AS1_T2_facilitator_time.txt'
-AS1_f_NT = 'AS1_T2_facilitator_noTS.txt'
-AS1_p_TS = 'AS1_T2_participant_time.txt'
-AS1_p_NT = 'AS1_T2_participant_noTS.txt'
-AS1_s_TS = 'AS1_T2_Stereo_time.txt'
-AS1_s_NT = 'AS1_T2_Stereo_noTS.txt'
-
-AS2_f_TS = 'AS2_T2_facilitator_time.txt'
-AS2_f_NT = 'AS2_T2_facilitator_noTS.txt'
-AS2_p_TS = 'AS2_T2_participant_time.txt'
-AS2_p_NT = 'AS2_T2_participant_noTS.txt'
-AS2_s_TS = 'AS2_T2_Stereo_time.txt'
-AS2_s_NT = 'AS2_T2_Stereo_noTS.txt'
-
-AS3_f_TS = 'AS3_T2_facilitator_time.txt'
-AS3_f_NT = 'AS3_T2_facilitator_noTS.txt'
-AS3_p_TS = 'AS3_T2_participant_time.txt'
-AS3_p_NT = 'AS3_T2_participant_noTS.txt'
-AS3_s_TS = 'AS3_T2_Stereo_time.txt'
-AS3_s_NT = 'AS3_T2_Stereo_noTS.txt'
-
-TD1_f_TS = 'TD1_T2_facilitator_time.txt'
-TD1_f_NT = 'TD1_T2_facilitator_noTS.txt'
-TD1_p_TS = 'TD1_T2_participant_time.txt'
-TD1_p_NT = 'TD1_T2_participant_noTS.txt'
-TD1_s_TS = 'TD1_T2_Stereo_time.txt'
-TD1_s_NT = 'TD1_T2_Stereo_noTS.txt'
-
-TD2_f_TS = 'TD2_T2_facilitator_time.txt'
-TD2_f_NT = 'TD2_T2_facilitator_noTS.txt'
-TD2_p_TS = 'TD2_T2_participant_time.txt'
-TD2_p_NT = 'TD2_T2_participant_noTS.txt'
-TD2_s_TS = 'TD2_T2_Stereo_time.txt'
-TD2_s_NT = 'TD2_T2_Stereo_noTS.txt'
-
-TD3_f_TS = 'TD3_T2_facilitator_time.txt'
-TD3_f_NS = 'TD3_T2_facilitator_noTS.txt'
-TD3_p_TS = 'TD3_T2_participant_time.txt'
-TD3_p_NS = 'TD3_T2_participant_noTS.txt'
-TD3_s_TS = 'TD3_T2_Stereo_time.txt'
-TD3_s_NS = 'TD3_T2_Stereo_noTS.txt'
+##### GLOBAL VARIABLES ##########################################
+startup_info = '#################################################################'
+startup_info +='\n# CLAAP - Corpus & Linguistics Annotating & Analyzing in Python #'
+startup_info +='\n# Version 0.00 \tJune 15, 2015 \t3:55 PM UTC \t\t\t#'
+startup_info +='\n# Developed by Benjamin S. Meyers\t\t\t\t#'
+startup_info +='\n#\t\t\t\t\t\t\t\t#'
+startup_info +='\n# This application may not be copied, altered, or distributed \t#'
+startup_info +='\n# without written consent from the product owner. \t\t#'
+startup_info +='\n# \t\t\t\t\t\t\t\t#'
+startup_info +='\n# Type "~" for more information. \t\t\t\t#'
+startup_info +='\n#\t\t\t\t\t\t\t\t#'
+startup_info +='\n# Welcome to CLAAP! Type "h" for help.\t\t\t\t#'
+startup_info +='\n#################################################################'
 
 exit_messages = [("If you're happy and you know it, CLAAP your hands!"),("Petrichor\n(noun)\na pleasant smell that frequently accompanies the first rain after a long period of warm, dry weather."), ("Syzygy is the only word in English that contains three 'y's."), ("Tmesis is the only word in the English language that begins with 'tm'."), ("In Old English, bagpipes were called 'doodle sacks'."), ("A 'quire' is two-dozen sheets of paper."), ("'Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo' is a grammatically correct sentence in American English."), ("J.R.R. Tolkien coined the term 'glossopoeia,' which is the act of inventing languages."), ("Beowulf is an English work, but if you try to read it in its original form, it will look like gibberish!"), ("'To Be Or Not To Be' = 'U+0032 U+0042 U+2228 U+0021 U+0032 U+0042'")]
 
 more_info = "\nThis application was developed by Benjamin S. Meyers beginning on June 1, 2015 for an Undergraduate Research Internship at the Rochester Institute of Technology. CLAAP's primary application is to allow an easy-to-use tool for Linguists to annotate and analyze corpora consisting of discourse and dialogue.\n\tVersion 0.00\tJune 15, 2015\t3:55 PM UTC\n\tVersion 0.01\tJune 16, 2015\t11:29 AM UTC"
 
-##### FUNCTIONS ##################################
+moby_dick = nltk.Text(gutenberg.words('melville-moby_dick.txt'))
+
+douglas = "\n           o o o   .-\'\"\"\"\'-.   o o o             DON\'T PANIC!"
+douglas +="\n           \\\|/  .'         '.  \|//"
+douglas +="\n            \-;o/             \o;-/"
+douglas +="\n            // ;               ; \\\\"
+douglas +="\n           //__; :.         .: ;__\\\\"
+douglas +="\n          `-----\\'.'-.....-'.'/-----'           444    2222222"
+douglas +="\n                 '.'.-.-,_.'.'                 4444   222   222"
+douglas +="\n                   '(  (..-'                  44 44         22"
+douglas +="\n      |              '-'                     44  44        22"
+douglas +="\n  |           |                             444444444     22"
+douglas +="\n |  |  |    |                                    44      22"
+douglas +="\n     |     |  |                                  44    222"
+douglas +="\n| |   |     %%%                                  44   222222222"
+douglas +="\n    ___    _\|/_         _%%_____"
+douglas +="\n\,-\' \'_|   \___/      __/___ \'   \\"                    
+douglas +="\n/\"\"----\'          ___/__  \'   \'\'  \__%__       __%____%%%___"
+douglas +="\n                 /   \" \'   _%__ \'   \'   \_____/____ \'  __ \" \\"
+douglas +="\n           __%%_/__\'\' __     \'   _%_\'_   \     \"\'    _%__ \'\' \_"
+douglas +="\n __/\__%%_/_/___\___ \'\'   \'_%_\"___   \"    \_%__ \'___\"     \"\'   \\"
+douglas +="\n/_________________\____\'_RIP Douglas___\"_______\_______\'_____\"__\\"
+douglas +="\n"
+
+##### FUNCTIONS #################################################
 
 #Display the top x most frequent tokens (words) in the given text_file.
 def get_freqs(text_file, x):
@@ -93,66 +90,15 @@ def get_tokens(text_file):
 
 	return tokens
 
-##### TEST FUNCTIONS #############################
-def check_tokenize_accuracy():
-	if get_tokens(AS1_f_NT) == get_tokens(AS1_f_NT):
-		return 'ACCURATE!'
-	else:
-		return 'INACCURATE!'
-
-def run_accuracy_tests():
-	print('##### RUNNING TESTS ####################')
-	print('\nget_tokens(str) status: ' + check_tokenize_accuracy())
-	print('\n########################################')
-
-##### DISPLAY COMMAND LIST #######################
+##### DISPLAY COMMAND LIST ######################################
 def display_command_list():
-	command_list = '#################################################################'
+	command_list = '##### COMMAND LIST ##############################################'
 	command_list += '\n# command \targ1 \targ2 \tdescription\t\t\t#'
 	command_list += '\n#\t\t\t\t\t\t\t\t#'
 	command_list += '\n# f \t\tstr \tint \tget_freqs(str, int)\t\t#'
 	command_list += '\n# h \t\t-- \t-- \tdisplay_command_list()\t\t#'
 	command_list += '\n# t \t\tstr \t-- \tget_tokens(str)\t\t\t#'
-	command_list += '\n# ! \t\t-- \t-- \trun_accuracy_tests()\t\t#'
+	command_list += '\n# ! \t\t-- \t-- \trun_tests()\t\t\t#'
 	command_list += '\n# q \t\t-- \t-- \tquit program.\t\t\t#'
 	command_list += '\n#################################################################'
 	print(command_list)
-
-##### MAIN #######################################
-def main():
-	print('#################################################################')
-	print('# CLAAP - Corpus & Linguistics Annotating & Analyzing in Python #')
-	print('# Version 0.00 \tJune 15, 2015 \t3:55 PM UTC \t\t\t#')
-	print('# Developed by Benjamin S. Meyers\t\t\t\t#')
-	print('#\t\t\t\t\t\t\t\t#')
-	print('# This application may not be copied, altered, or distributed \t#')
-	print('# without written consent from the product owner. \t\t#')
-	print('# \t\t\t\t\t\t\t\t#')
-	print('# Type "~" for more information. \t\t\t\t#')
-	print('#\t\t\t\t\t\t\t\t#')
-	print('# Welcome to CLAAP! Type "h" for help.\t\t\t\t#')
-	print('#################################################################')
-	while(True):
-		user_command = raw_input('Please provide a command: ')
-		command = word_tokenize(user_command)
-		#print(command)
-		if command[0] == 't' and len(command) == 2:
-			get_tokens(command[1])
-		elif command[0] == 'f' and len(command) == 3:
-			get_freqs(command[1], int(command[2]))
-		elif command[0] == 'h' and len(command) == 1:
-			display_command_list()
-		elif command[0] == '!' and len(command) == 1:
-			run_accuracy_tests()
-		elif command[0] == '~' and len(command) == 1:
-			print(more_info)
-		elif len(command) == 1 and command[0] == '42':
-			print('\nLook at you, getting all existential on me!\n')
-		elif command[0] == 'q' and len(command) == 1:
-			print('\n' + random.choice(exit_messages) + '\n')
-			break
-		else:
-			print('Invalid Command.\nFor Help, type \'h\'.')
-
-if __name__ == "__main__":
-    main()
