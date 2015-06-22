c246029159e13bf0006cc50a5f5b4df167b90d8b
Benjamin Meyers <meyersbs@users.noreply.github.com>
2015-06-22 12:00:18 -0400

    Implemented Type-Token Ratio and filename aliases.

--- nltk_main.py
+++ nltk_main.py
@@ -13,6 +13,9 @@ from nltk_functions import *
 from nltk_variables import *
 from nltk_tests import run_tests
 
+##### ALIASES ###################################################
+file_aliases = build_alias_dict()
+
 ##### MAIN ######################################################
 def main():
 	print(startup_info)
@@ -26,29 +29,53 @@ def main():
 		#print(get_lexical_diversity('moby_dick.txt'))
 		#print(get_word_count('moby_dick.txt'))
 		if command[0] == 'tk' and len(command) == 2:
-			print(get_tokens(command[1]))
+			if file_aliases[command[1]] != None:
+				print(get_tokens(file_aliases[command[1]]))
+			else:
+				print(get_tokens(command[1]))
 		elif command[0] == 'ty' and len(command) == 2:
-			print(get_types(command[1]))
+			if file_aliases[command[1]] != None:
+				print(get_types(file_aliases[command[1]]))
+			else:
+				print(get_types(command[1]))
 		elif command[0] == 'wc' and len(command) == 2:
-			print(get_word_count(command[1]))
+			if file_aliases[command[1]] != None:
+				print(get_word_count(file_aliases[command[1]]))
+			else:
+				print(get_word_count(command[1]))
 		elif command[0] == 'uwc' and len(command) == 2:
-			print(get_unique_word_count(command[1]))
-		elif command[0] == 'ld' and len(command) == 2:
-			print(get_lexical_diversity(command[1]))
+			if file_aliases[command[1]] != None:
+				print(get_unique_word_count(file_aliases[command[1]]))
+			else:
+				print(get_unique_word_count(command[1]))
+		elif command[0] == 'ttr' and len(command) == 2:
+			if file_aliases[command[1]] != None:
+				print(str(get_TTR(file_aliases[command[1]])) + '%')
+			else:
+				print(str(get_TTR(command[1])) + '%')
+		elif command[0] == 'pfd' and len(command) == 2:
+			if file_aliases[command[1]] != None:
+				plot_freq_dist(file_aliases[command[1]], -1)
+			else:
+				plot_freq_dist(command[1], -1)
+		elif command[0] == 'pos' and len(command) == 2:
+			if file_aliases[command[1]] != None:
+				print(tag_parts_of_speech(file_aliases[command[1]]))
+			else:
+				print(tag_parts_of_speech(command[1]))
+		elif command[0] == 'psc' and len(command) == 2:
+			if file_aliases[command[1]] != None:
+				print(get_pos_counts(file_aliases[command[1]]))
+			else:
+				print(get_pos_counts(command[1]))
 		elif command[0] == 'mf' and len(command) == 3:
 			print(get_most_frequent(command[1], int(command[2])))
 		elif command[0] == 'lf' and len(command) == 3:
 			print(get_least_frequent(command[1], int(command[2])))
 		elif command[0] == 'pfd' and len(command) == 3:
 			plot_freq_dist(command[1], int(command[2]))
-		elif command[0] == 'pfd' and len(command) == 2:
-			plot_freq_dist(command[1], -1)
 		elif command[0] == 's' and len(command) == 3:
 			print(look_up_word(command[1], command[2]))
-		elif command[0] == 'pos' and len(command) == 2:
-			print(tag_parts_of_speech(command[1]))
-		elif command[0] == 'psc' and len(command) == 2:
-			print(get_pos_counts(command[1]))
 		elif command[0] == 'h' and len(command) == 1:
 			display_command_list()
 		elif command[0] == '!' and len(command) == 1:
