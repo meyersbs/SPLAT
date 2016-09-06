b8fccfb7d9df3f86e1d81bf41f9d674d2f48d19a
meyersbs <meyers.bs@gmail.com>
2016-09-06 11:55:11 -0400

    Updates for pypi.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='SPLAT-library',
-    version='0.1.5',
+    version='0.1.6',
     description='Speech Processing & Linguistic Analysis Tool',
     long_description="SPLAT is a command-line application designed to make it easy for linguists (both computer-oriented and non-computer-oriented) to use the Natural Language Tool Kit (NLTK) for analyzing virtually any text file.\n\nSPLAT is designed to help you gather linguistic features from text files and it is assumed that most input files will not be already annotated. In order for SPLAT to function properly, you should ensure that the input files that you provide do not contain any annotations. Because there are so many variations of linguistic annotation schemes, it would simply be impossible to account for all of them in the initial parsing of input files; it is easier for you to remove any existing annotations than it is for me to do so.",
     url='http://splat-library.org',
     author='Benjamin S. Meyers',
     author_email='ben@splat-library.org',
     license='MIT',
-    scripts=['install.sh'],
+    scripts=['splat/base/splat-cli'],
     keywords=['nlp', 'natural language', 'natural language processing'],
     packages=[
         'splat',
@@ -25,7 +25,7 @@ setup(
         'splat.tokenizers'
     ],
     install_requires=['matplotlib', 'nltk'],
-    download_url='https://github.com/meyersbs/SPLAT/tarball/0.1.5',
+    download_url='https://github.com/meyersbs/SPLAT/tarball/0.1.6',
     requires=['matplotlib', 'nltk'],
     classifiers=[
         'Development Status :: 3 - Alpha',
