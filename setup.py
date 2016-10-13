3eb0124108d36f668f7dfdda568a5877c6d82bc1
Benjamin Meyers <ben@lionlogic.org>
2016-10-13 15:36:29 -0400

    Working on making PyPi install NLTK data dependencies.

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from distutils.core import setup
+from distutils.command.install_data import install_data
+import subprocess
+
+class MyInstall(install_data):
+    def run(self):
+        install_data.run(self)
+        subprocess.call("python3 -m nltk.downloader stopwords", stdin=None, stdout=None, stderr=None, shell=False, timeout=None)
+        subprocess.call("python3 -m nltk.downloader names", stdin=None, stdout=None, stderr=None, shell=False, timeout=None)
+        subprocess.call("python3 -m nltk.downloader punkt", stdin=None, stdout=None, stderr=None, shell=False, timeout=None)
+        subprocess.call("python3 -m nltk.downloader averaged_perceptron_tagger", stdin=None, stdout=None, stderr=None, shell=False, timeout=None)
 
 setup(
     name='SPLAT-library',
-    version='0.2.3',
+    version='0.2.4',
     description='Speech Processing & Linguistic Analysis Tool',
     long_description="SPLAT is a command-line application designed to make it easy for linguists (both computer-oriented and non-computer-oriented) to use the Natural Language Tool Kit (NLTK) for analyzing virtually any text file.\n\nSPLAT is designed to help you gather linguistic features from text files and it is assumed that most input files will not be already annotated. In order for SPLAT to function properly, you should ensure that the input files that you provide do not contain any annotations. Because there are so many variations of linguistic annotation schemes, it would simply be impossible to account for all of them in the initial parsing of input files; it is easier for you to remove any existing annotations than it is for me to do so.",
     url='http://splat-library.org',
@@ -12,7 +22,6 @@ setup(
     scripts=['splat/base/splat-cli'],
     keywords=['nlp', 'natural language', 'natural language processing'],
     package_data={'splat.parse': ['BerkeleyParser-1.7.jar', 'eng_sm6.gr']},
-    #data_files=[('splat', ['parse/BerkeleyParser-1.7.jar', 'parse/eng_sm6.gr'])],
     packages=[
         'splat',
         'splat.annotation',
@@ -26,11 +35,13 @@ setup(
         'splat.tag',
         'splat.tokenizers'
     ],
-    download_url='https://github.com/meyersbs/SPLAT/archive/v0.2.3.tar.gz',
+    download_url='https://github.com/meyersbs/SPLAT/archive/v0.2.4.tar.gz',
     requires=['matplotlib', 'nltk'],
+    cmdclass={'install': MyInstall},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
         'Topic :: Text Processing :: Linguistic',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
