1a5bb335a62fdb6aa15fc894535bcbb19ff642b4
Benjamin Meyers <ben@lionlogic.org>
2016-10-13 20:05:41 -0400

    Replacing pickle with jsonpickle.

--- splat/__init__.py
+++ splat/__init__.py
@@ -10,14 +10,56 @@
 ### @LICENSE_TYPE:		MIT																							 ###
 ########################################################################################################################
 ########################################################################################################################
+import subprocess
+from subprocess import CompletedProcess
 
 try:
     import nltk
-    from splat import SPLAT
-    nltk.download("stopwords")
-    nltk.download("names")
-    nltk.download("punkt")
-    nltk.download("averaged_perceptron_tagger")
-except:
-    print(
-        "WARNING: Essential NLTK data could not be installed. Please run the following:\n python3 -m nltk.downloader stopwords names punkt averaged_perceptron_tagger")
+except ImportError:
+    print("Oops! It looks like NLTK was not installed. Let's fix that.")
+    print("Installing NLTK...")
+    subprocess.run("pip3 install nltk", shell=False, check=False)
+    if subprocess.CompletedProcess.returncode == 0:
+        print("NLTK was successfully installed!")
+    else:
+        print("Hmm... I couldn't install NLTK for you. You probably don't have root privileges. I suggest running this command:\n\tsudo pip3 install nltk")
+
+try:
+    from nltk.corpus import stopwords
+    from nltk.corpus import names
+    from nltk.tokenize import punkt
+    from nltk import pos_tag
+except ImportError:
+    print("Oops! It looks like some essential NLTK was not downloaded. Let's fix that.")
+    print("Downloading NLTK data...")
+    subprocess.run("python3 -m nltk.downloader stopwords names punkt averaged_perceptron_tagger", shell=False, check=False)
+    if subprocess.CompletedProcess.returncode == 0:
+        print("Essential NLTK data was successfully downloaded!")
+    else:
+        print("Hmm... I couldn't download the essential NLTK data for you. I suggest running this command:\n\tpython3 -m nltk.downloader stopwords names punkt averaged_perceptron_tagger")
+
+try:
+    import matplotlib
+except ImportError:
+    print("Oops! It looks like matplotlib was not installed. Let's fix that.")
+    print("Installing matplotlib...")
+    subprocess.run("pip3 install matplotlib", shell=False, check=False)
+    if subprocess.CompletedProcess.returncode == 0:
+        print("matplotlib was successfully installed!")
+    else:
+        print("Hmm... I couldn't install matplotlib for you. You probably don't have root privileges. I suggest running this command:\n\tsudo pip3 install matplotlib")
+
+try:
+    import jsonpickle
+except ImportError:
+    print("Oops! It looks like jsonpickle was not installed. Let's fix that.")
+    print("Installing jsonpickle...")
+    subprocess.run("pip3 install jsonpickle", shell=False, check=False)
+    if subprocess.CompletedProcess.returncode == 0:
+        print("jsonpickle was successfully installed!")
+    else:
+        print("Hmm... I couldn't install jsonpickle for you. You probably don't have root privileges. I suggest running this command:\n\tsudo pip3 install jsonpickle")
+
+java_status = subprocess.call(["which", "java"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+if java_status != 0:
+    print("Java is not installed on your system. Java needs to be installed in order for me to do any part-of-speech tagging.\n\nPlease install java and try again.")
